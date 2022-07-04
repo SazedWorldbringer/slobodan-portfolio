ee4e6aa9da8ca8c0639718d4d2c4029890a2823f
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-04 08:28:03 +0530

    feat: write layout component and make styles file for layout component

---
+++ src/components/Layout/layout.component.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../Sidebar/sidebar.component";
+import "./layout.styles.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Layout;
