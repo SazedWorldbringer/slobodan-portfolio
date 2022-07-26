4055b836d1cf583185562db08181ad3dd817f98d
Atharva Pardeshi <atharva.exe@gmail.com>
2022-07-26 18:32:40 +0530

    feat: add social links

--- src/components/Sidebar/sidebar.component.jsx
+++ src/components/Sidebar/sidebar.component.jsx
@@ -3,7 +3,17 @@ import { Link, NavLink } from "react-router-dom";
 import "./sidebar.styles.scss";
 import LogoS from "../../assets/images/logo-s.png";
 import LogoSubtitle from "../../assets/images/logo_sub.png";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEnvelope,
+  faFile,
+  faFileAlt,
+  faFileCsv,
+  faFilePen,
+  faFileText,
+  faHome,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -34,6 +44,37 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"></FontAwesomeIcon>
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/atharvapardeshi/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/SazedWorldbringer"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/SazedWorldbringer"
+        >
+          <FontAwesomeIcon icon={faFileText} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
